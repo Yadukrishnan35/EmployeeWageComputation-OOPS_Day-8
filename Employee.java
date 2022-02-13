fc267cd4d12a17be15fd56f1d06aa6ed5bd469ad
yadukrishnan <yadulive333@gmail.com>
2022-02-13 23:16:52 +0530

    Employee Presence

---
+++ Employee.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		empcheck();
+
+	}
+
+	public static void empcheck()
+	{
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		System.out.println("empCheck"+empCheck);
+		int PRESENT = 1;
+
+		if(empCheck == PRESENT)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+	
+
+
+
+}
