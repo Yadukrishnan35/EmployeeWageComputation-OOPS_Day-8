7e9248cc9544121693671b3be98fd2829e897a6b
yadukrishnan <yadulive333@gmail.com>
2022-02-14 09:23:56 +0530

    Employee Presence

--- Employee.java
+++ Employee.java
@@ -2,26 +2,19 @@ package com.bridgelabz;
 
 public class Employee {
 
+    static  int PRESENT = 1;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		
 		empcheck();
-
-	}
+}
 
 	public static void empcheck()
 	{
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		System.out.println("empCheck"+empCheck);
-		int PRESENT = 1;
-
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
 		if(empCheck == PRESENT)
 			System.out.println("Employee is Present");
 		else
 			System.out.println("Employee is Absent");
 	}
 	
-
-
-
 }
---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Employee emp = new Employee();
+        Employee.empcheck();
+	}
+
+}
