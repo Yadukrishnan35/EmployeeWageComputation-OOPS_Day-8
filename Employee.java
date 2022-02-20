4eb6df3c6787d181d38ce47d052c1c238e108679
yadukrishnan <yadulive333@gmail.com>
2022-02-20 21:18:56 +0530

    Check Employee Attendence

--- Employee.java
+++ Employee.java
@@ -1,20 +1,13 @@
 package com.bridgelabz;
-
 public class Employee {
-
-    static  int PRESENT = 1;
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-		empcheck();
-}
-
-	public static void empcheck()
-	{
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-		if(empCheck == PRESENT)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+	 
+	int IS_PRESENT = 1;
+	 
+	 public void checkIsPresent(double empCheck){
+	        if(empCheck == IS_PRESENT)
+	            System.out.println("Employee is Present");
+	        else
+	            System.out.println("Employee is Absent"); 
 	}
-	
+
 }
--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ package com.bridgelabz;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
         Employee emp = new Employee();
-        Employee.empcheck();
+        double empCheck = Math.floor (Math.random() * 10) % 2;
+        emp.checkIsPresent(empCheck);
 	}
 
 }
