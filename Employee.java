42a4c7668e5728aa62c83ed20f8af5c6a47948cb
yadukrishnan <yadulive333@gmail.com>
2022-02-22 12:36:34 +0530

    Monthly Wage with condition

--- Employee.java
+++ Employee.java
@@ -2,45 +2,56 @@ package com.bridgelabz;
 
 public class Employee {
 	 
-	    final int IS_PRESENT = 1;
-	    final int FULL_TIME = 1;
-	    final int WORKING_DAYS = 20;
-	    int fullDayHr= 16;
-	    int wagePerHr = 20;
-	    
-	    public boolean checkIsPresent(int empCheck){
-	        if(empCheck == IS_PRESENT){ 
-	            return true;
-	        }
-	        else{ 
-	            return false;
-	        }
-	    }
-	    public int calcDailyEmpWage(int empType){
+	final int IS_PRESENT = 1;
+    final int FULL_TIME = 1;
+    int FULL_DAY_HR= 16;
+    int workingDays;
+    int wagePerHr;
+    int totalAttendance = 0;
+    int totalHr = 0;
+    
+    public Employee(int workingDays , int wagePerHr){
+        this.workingDays = workingDays;
+        this.wagePerHr = wagePerHr;
+    }
+    public boolean checkIsPresent(int empCheck){
+        if(empCheck == IS_PRESENT){ 
+            totalAttendance++;
+            return true;
+        }
+        else{ 
+            return false;
+        }
+    }
+    public int calcDailyEmpWage(int empType){
 
-	         int dailyEmpWage = 0;
-	        switch(empType){
-	            case  FULL_TIME : 
-	                dailyEmpWage =  wagePerHr * fullDayHr;
-	            break;
-	            default : 
-	                dailyEmpWage = dailyEmpWage / 2;
-	                
-	        }
-	        return dailyEmpWage;
-	    }
-	    public int calcMonthlyWage(){
+        int dailyEmpWage = 0;
+        switch(empType){
+            case  FULL_TIME :
+                totalHr += FULL_DAY_HR;
+                dailyEmpWage =  wagePerHr * FULL_DAY_HR;
+            break;
+            default :
+                totalHr += FULL_DAY_HR / 2; 
+                dailyEmpWage = dailyEmpWage / 2;
+        }
+        return dailyEmpWage;
+    }
+    public int calcMonthlyWage(){
 
-	        int monthlyWage = 0;
-	        int dailyWage;
-	        for(int day=1; day<=WORKING_DAYS; day++){
-	            int empCheck = (int)(Math.random() * 10) % 2;
-	            if(checkIsPresent(empCheck)){    
-	                int empType = (int)(Math.random() * 10) % 2;
-	                dailyWage = calcDailyEmpWage(empType);   
-	                monthlyWage += dailyWage;
-	            }
-	        }
-	        return monthlyWage;
-	    }
-}
+        int monthlyWage = 0;
+        int dailyWage;
+    
+        while(totalAttendance <=20 && totalHr <=100){
+            for(int day=1; day<=workingDays; day++){
+                int empCheck = (int)(Math.random() * 10) % 2;
+                if(checkIsPresent(empCheck)){
+                    int empType = (int)(Math.random() * 10) % 2;
+                    dailyWage = calcDailyEmpWage(empType);   
+                    monthlyWage += dailyWage;
+                }
+            }
+        }
+        return monthlyWage;
+    }
+    }
--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		    Employee emp = new Employee();
+		    Employee emp = new Employee(20,20);
 	        int wage = emp.calcMonthlyWage();
 	        System.out.println("Monthly wage :" + wage);
 	}
