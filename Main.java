be3baf352988a5495453858cafe1ba15acb81349
yadukrishnan <yadulive333@gmail.com>
2022-02-22 10:12:55 +0530

    Using switch case

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Employee emp = new Employee();
-        double empCheck = Math.floor (Math.random() * 10) % 2;
-        double empType = Math.floor (Math.random() * 10) % 2;
+        int empCheck = (int)(Math.random() * 10) % 2;
+        int empType = (int)(Math.random() * 10) % 2;
         if(emp.checkIsPresent(empCheck)){
             emp.calcDailyEmpWage(empType);
         }
