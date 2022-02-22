14632bf48e850459dad93cad649b078bedc5314d
yadukrishnan <yadulive333@gmail.com>
2022-02-22 12:00:37 +0530

    Find Monthly Wage

--- Main.java
+++ Main.java
@@ -3,15 +3,10 @@ package com.bridgelabz;
 public class Main {
 	
 	public static void main(String[] args) {
-		Employee emp = new Employee();
-        int empCheck = (int)(Math.random() * 10) % 2;
-        int empType = (int)(Math.random() * 10) % 2;
-        if(emp.checkIsPresent(empCheck)){
-            emp.calcDailyEmpWage(empType);
-        }
-        else{
-            System.out.println("Employee is absent");
-        }
+		
+		    Employee emp = new Employee();
+	        int wage = emp.calcMonthlyWage();
+	        System.out.println("Monthly wage :" + wage);
 	}
 
 }
