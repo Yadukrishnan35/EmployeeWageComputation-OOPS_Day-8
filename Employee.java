feb33eb8f52062184f5ebdab7f83a68874499f58
yadukrishnan <yadulive333@gmail.com>
2022-02-22 09:11:52 +0530

    Find Part time Daily Wage

--- Employee.java
+++ Employee.java
@@ -3,17 +3,24 @@ package com.bridgelabz;
 public class Employee {
 	 
 	  int IS_PRESENT = 1;
-	  int WAGE_PER_HR=20;
-	  int FULL_DAY_HR=8;  
+	  int WAGE_PER_HR = 20;
+	  int FULL_DAY_HR = 8;  
+	  int FULLTIME = 1;
 	  public boolean checkIsPresent(double empCheck){
 	        if(empCheck == IS_PRESENT)
 	            return true;
 	        else
 	            return false; 
-	    }
-
-	  public int calcDailyEmpWage(){
+	  }
+	  public void calcDailyEmpWage(double emptype){
 	        int dailyEmpWage =  WAGE_PER_HR * FULL_DAY_HR;
-	        return  dailyEmpWage; 
-}
+	        if(emptype == FULLTIME) {
+	        	System.out.println("Employee type : Full time");
+	        	System.out.println("Daily Employee wage :" +dailyEmpWage);
+	        }
+	        else {
+	        	System.out.println("Employee type : Part time");
+	        	System.out.println("Daily Employee Wage :" + (dailyEmpWage/2));
+	        }
+	  }
 }
--- Main.java
+++ Main.java
@@ -5,15 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		Employee emp = new Employee();
         double empCheck = Math.floor (Math.random() * 10) % 2;
+        double empType = Math.floor (Math.random() * 10) % 2;
         if(emp.checkIsPresent(empCheck)){
-            
-            System.out.println("Daily Employee Wage :" +  emp.calcDailyEmpWage());
+            emp.calcDailyEmpWage(empType);
         }
         else{
             System.out.println("Employee is absent");
         }
-	
-	
 	}
 
 }
